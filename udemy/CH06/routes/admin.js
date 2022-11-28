c8724832127830b4d39e44fdf98969d4d528032a
EastZero123 <cdy1005p@naver.com>
2022-11-28 21:31:44 +0900

    CH06 checkpoint

--- udemy/CH06/routes/admin.js
+++ udemy/CH06/routes/admin.js
@@ -1,20 +1,23 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- udemy/CH06/routes/shop.js
+++ udemy/CH06/routes/shop.js
@@ -1,13 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  res.render("shop");
 });
 
 module.exports = router;
