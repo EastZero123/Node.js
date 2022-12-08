8301c32f94eb4d1ed79a6142f3349c51ff2d8a9e
EastZero123 <cdy1005p@naver.com>
2022-12-08 22:45:31 +0900

    CH09 checkpoint

--- udemy/CH09/controllers/shop.js
+++ udemy/CH09/controllers/shop.js
@@ -12,8 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {});
-  res.redirect("/");
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -31,6 +36,11 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {
