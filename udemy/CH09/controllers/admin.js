dbdceaaf49823e3b865a6f37a561b38c21182314
EastZero123 <cdy1005p@naver.com>
2022-12-09 23:27:09 +0900

    CH09

--- udemy/CH09/controllers/admin.js
+++ udemy/CH09/controllers/admin.js
@@ -1,12 +1,9 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
   });
 };
 
@@ -17,15 +14,27 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
+  res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    res.redirect("/");
+  }
+  res.render("admin/edit-product", {
+    pageTitle: "Edit Product",
+    path: "/admin/edit-product",
+    editing: editMode,
+  });
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
   });
 };
---
+++ udemy/CH09/models/cart.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  // constructor() {
+  //     this.products = [];
+  //     this.totalPrice = 0;
+  // }
+
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updateProduct;
+      if (existingProduct) {
+        updateProduct = { ...existingProduct };
+        updateProduct.qty = updateProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updateProduct;
+      } else {
+        updateProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updateProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {});
+    });
+  }
+};
