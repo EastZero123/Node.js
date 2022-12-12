30939ab6a43a92c9757a3fd8bffc118922567605
EastZero123 <cdy1005p@naver.com>
2022-12-12 21:43:40 +0900

    CH11 checkpoint

---
+++ udemy/CH11/util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "1234", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
