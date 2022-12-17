88cac6b3196f5ecd55f6bbc8464a0fc362b4c859
EastZero123 <cdy1005p@naver.com>
2022-12-17 21:45:04 +0900

    CH 06

---
+++ gilbut/CH6/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    //   res.send("Hello, Express");
+    res.sendFile(path.join(__dirname, "/index.html"));
+    console.log("GET / 에만 실행");
+    enxt();
+  },
+  (req, res) => {
+    throw new Error("에러는 에러 처리 미들웨어");
+  }
+);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
