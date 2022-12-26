a174c6e81b456988acafc9b5a0fac228cf78ef4a
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2022-12-26 16:56:34 +0900

    CH5 checkpoint

--- udemy/MERN/CH5/src/App.js
+++ udemy/MERN/CH5/src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react"
+import { Redirect, Route, Router, Switch } from "react-router-dom"
+
+import NewPlace from "./places/pages/NewPlace"
+import Users from "./user/pages/Users"
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  )
 }
-
-export default App;
+export default App
