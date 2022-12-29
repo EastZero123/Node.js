7a9a991e623396ce184017f1d0f155e056305b75
EastZero123 <cdy1005p@naver.com>
2022-12-29 22:38:59 +0900

    Update user.js

--- udemy/NodeJS/CH12/models/user.js
+++ udemy/NodeJS/CH12/models/user.js
@@ -4,14 +4,30 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProduct = this.cart.items.findIndex((cp) => {
+      return cp._id === product._id;
+    });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
