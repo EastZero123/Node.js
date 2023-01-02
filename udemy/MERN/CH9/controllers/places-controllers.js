3efe0743b1f3c4444593a6014bf7748394fafb12
EastZero123 <cdy1005p@naver.com>
2023-01-02 20:39:36 +0900

    CH9 finish

--- udemy/MERN/CH9/controllers/places-controllers.js
+++ udemy/MERN/CH9/controllers/places-controllers.js
@@ -1,85 +1,87 @@
-const uuid = require("uuid/v4")
-const { validationResult } = require("express-validator")
+const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 
-const HttpError = require("../models/http-error")
-const getCoordsForAddress = require("../util/location")
-const Place = require("../models/place")
-const User = require("../models/user")
-const { default: mongoose } = require("mongoose")
+const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
+const Place = require("../models/place");
+const User = require("../models/user");
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-]
+// let DUMMY_PLACES = [
+//   {
+//     id: "p1",
+//     title: "Empire State Building",
+//     description: "One of the most famous sky scrapers in the world!",
+//     location: {
+//       lat: 40.7484474,
+//       lng: -73.9871516,
+//     },
+//     address: "20 W 34th St, New York, NY 10001",
+//     creator: "u1",
+//   },
+// ];
 
 const getPlaceById = async (req, res, next) => {
-  const placeId = req.params.pid // { pid: 'p1' }
+  const placeId = req.params.pid; // { pid: 'p1' }
 
-  let place
+  let place;
   try {
-    place = await Place.findById(placeId)
+    place = await Place.findById(placeId);
   } catch (err) {
-    const error = new HttpError("Something wrong")
-    return next(error)
+    const error = new HttpError("Something wrong");
+    return next(error);
   }
 
   if (!place) {
     const error = new HttpError(
       "Could not find a place for the provided id.",
       404
-    )
-    return next(error)
+    );
+    return next(error);
   }
 
-  res.json({ place: place.toObject({ getters: true }) }) // => { place } => { place: place }
-}
+  res.json({ place: place.toObject({ getters: true }) }); // => { place } => { place: place }
+};
 
 // function getPlaceById() { ... }
 // const getPlaceById = function() { ... }
 
 const getPlacesByUserId = async (req, res, next) => {
-  const userId = req.params.uid
+  const userId = req.params.uid;
 
-  let places
+  let places;
   try {
-    places = await Place.find({ creator: userId })
+    places = await User.findById(userId).populate("places");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 
   if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
-    )
+    );
   }
 
-  res.json({ places: places.map((place) => place.toObject({ getters: true })) })
-}
+  res.json({
+    places: places.map((place) => place.toObject({ getters: true })),
+  });
+};
 
 const createPlace = async (req, res, next) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
       new HttpError("Invalid inputs passed, please check your data.", 422)
-    )
+    );
   }
 
-  const { title, description, address, creator } = req.body
+  const { title, description, address, creator } = req.body;
 
-  let coordinates
+  let coordinates;
   try {
-    coordinates = await getCoordsForAddress(address)
+    coordinates = await getCoordsForAddress(address);
   } catch (error) {
-    return next(error)
+    return next(error);
   }
 
   // const title = req.body.title;
@@ -90,78 +92,83 @@ const createPlace = async (req, res, next) => {
     location: coordinates,
     image: "",
     creator,
-  })
+  });
 
-  let user
+  let user;
 
   try {
-    user = await User.findById(creator)
+    user = await User.findById(creator);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 
   try {
     // await createdPlace.save()
-    const sess = await mongoose.startSession()
-    sess.startTransaction()
-    await createdPlace.save({ session: sess })
-    user.places.push(createdPlace)
-    await user.save({ session: sess })
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdPlace.save({ session: sess });
+    user.places.push(createdPlace);
+    await user.save({ session: sess });
   } catch (error) {
-    const Error = new HttpError("Create Fail", 500)
-    return next(Error)
+    const Error = new HttpError("Create Fail", 500);
+    return next(Error);
   }
 
-  res.status(201).json({ place: createdPlace })
-}
+  res.status(201).json({ place: createdPlace });
+};
 
 const updatePlace = async (req, res, next) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
       new HttpError("Invalid inputs passed, please check your data.", 422)
-    )
+    );
   }
 
-  const { title, description } = req.body
-  const placeId = req.params.pid
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
 
-  let place
+  let place;
   try {
-    place = await Place.findById(placeId)
+    place = await Place.findById(placeId);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  place.title = title
-  place.description = description
+  place.title = title;
+  place.description = description;
 
   try {
-    await place.save()
+    await place.save();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 
-  res.status(200).json({ place: place.toObject({ getters: true }) })
-}
+  res.status(200).json({ place: place.toObject({ getters: true }) });
+};
 
 const deletePlace = async (req, res, next) => {
-  const placeId = req.params.pid
-  let place
+  const placeId = req.params.pid;
+  let place;
   try {
-    place = await Place.findById(placeId)
+    place = await Place.findById(placeId).populate("creator");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
   try {
-    await place.remove()
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  res.status(200).json({ message: "Deleted place." })
-}
+  res.status(200).json({ message: "Deleted place." });
+};
 
-exports.getPlaceById = getPlaceById
-exports.getPlacesByUserId = getPlacesByUserId
-exports.createPlace = createPlace
-exports.updatePlace = updatePlace
-exports.deletePlace = deletePlace
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- udemy/MERN/CH9/controllers/users-controllers.js
+++ udemy/MERN/CH9/controllers/users-controllers.js
@@ -1,48 +1,48 @@
-const uuid = require("uuid/v4")
-const { validationResult } = require("express-validator")
+const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
 
-const HttpError = require("../models/http-error")
-const User = require("../models/user")
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Max Schwarz",
-    email: "test@test.com",
-    password: "testers",
-  },
-]
+// const DUMMY_USERS = [
+//   {
+//     id: "u1",
+//     name: "Max Schwarz",
+//     email: "test@test.com",
+//     password: "testers",
+//   },
+// ]
 
 const getUsers = async (req, res, next) => {
-  let users
+  let users;
 
   try {
-    users = await User.find({}, "-password")
+    users = await User.find({}, "-password");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  res.json({ users: users.map((user) => user.toObject({ getters: true })) })
-}
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
+};
 
 const signup = async (req, res, next) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
       new HttpError("Invalid inputs passed, please check your data.", 422)
-    )
+    );
   }
-  const { name, email, password } = req.body
+  const { name, email, password } = req.body;
 
-  let existingUser
+  let existingUser;
   try {
-    existingUser = await User.findOne({ email: email })
+    existingUser = await User.findOne({ email: email });
   } catch (error) {
-    const Error = new HttpError("sign up fail", 500)
-    return next(Error)
+    const Error = new HttpError("sign up fail", 500);
+    return next(Error);
   }
   if (existingUser) {
-    const error = new HttpError("User exist", 422)
-    return next(error)
+    const error = new HttpError("User exist", 422);
+    return next(error);
   }
 
   const createdUser = new User({
@@ -51,40 +51,40 @@ const signup = async (req, res, next) => {
     image: "",
     password,
     places: [],
-  })
+  });
 
   try {
-    await createdUser.save()
+    await createdUser.save();
   } catch (error) {
-    const Error = new HttpError("Create Fail", 500)
-    return next(Error)
+    const Error = new HttpError("Create Fail", 500);
+    return next(Error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) })
-}
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+};
 
 const login = async (req, res, next) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
-  let existingUser
+  let existingUser;
   try {
-    existingUser = await User.findOne({ email: email })
+    existingUser = await User.findOne({ email: email });
   } catch (error) {
-    const Error = new HttpError("login fail", 500)
-    return next(Error)
+    const Error = new HttpError("login fail", 500);
+    return next(Error);
   }
   if (existingUser) {
-    const error = new HttpError("User exist", 422)
-    return next(error)
+    const error = new HttpError("User exist", 422);
+    return next(error);
   }
 
   if (existingUser || existingUser.password !== password) {
-    const Error = new HttpError("Invalid fail", 401)
-    return next(Error)
+    const Error = new HttpError("Invalid fail", 401);
+    return next(Error);
   }
-  res.json({ message: "Logged in!" })
-}
+  res.json({ message: "Logged in!" });
+};
 
-exports.getUsers = getUsers
-exports.signup = signup
-exports.login = login
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
